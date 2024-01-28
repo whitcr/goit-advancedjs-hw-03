eafd144ae952081148ec90f62ec2276ffdf28799
whitcr <whitcr0k@gmail.com>
2024-01-28 23:08:55 +0100

    added task

---
+++ src/cat-api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] =
+  'live_bibxAUJAz8ZnVN7nBo99DahMQ8LIldB1UtE4SMgluyM8fcqQ7jRbU71bWwMiMMPO';
+
+export { fetchBreeds, fetchCatByBreed };
+
+function fetchBreeds() {
+  return axios
+    .get(`${BASE_URL}/breeds`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching breeds:', error.message);
+      throw error;
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  const params = new URLSearchParams({ breed_ids: breedId });
+  return axios
+    .get(`${BASE_URL}/images/search?${params}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching info:', error.message);
+      throw error;
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,76 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+import iziToast from 'izitoast';
+import SlimSelect from 'slim-select';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'slim-select/styles';
+
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const catInfo = document.querySelector('.cat-info');
+
+fetchBreeds()
+  .then(data => {
+    createOptions(data);
+    new SlimSelect({
+      select: '.breed-select',
+    });
+  })
+  .catch(error => {
+    setLoader('none');
+    getError();
+  });
+
+function createOptions(breeds) {
+  try {
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+
+    setCatInfo('flex');
+    setLoader('none');
+  } catch (error) {
+    setLoader('none');
+    getError();
+  }
+}
+
+breedSelect.addEventListener('change', addInfo);
+
+function addInfo(event) {
+  setCatInfo('none');
+  setLoader('block');
+  const selectedBreedId = event.target.value;
+  fetchCatByBreed(selectedBreedId).then(data => {
+    const arr = data[0].breeds[0];
+
+    catInfo.innerHTML = `
+      <img class="cat-image" src="${data[0].url}" alt="Cat Image">
+      <div class="cat-content">
+      <h1><strong> ${arr.alt_names}</strong> </h1>
+      <p>  ${arr.description} </p>
+      <p><strong>Temperament:</strong> ${arr.temperament}</p></div>
+    `;
+
+    setLoader('none');
+    setCatInfo('flex');
+  });
+}
+
+function getError() {
+  iziToast.error({
+    title: 'Error',
+    message: 'Oops! Something went wrong! Try reloading the page!',
+  });
+}
+
+function setLoader(atr) {
+  loader.style.display = atr;
+}
+
+function setCatInfo(atr) {
+  catInfo.style.display = atr;
+}
